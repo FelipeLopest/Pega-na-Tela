95912bd956e0d2935fcde318c2bcab1ab52e90b7
felipe <felipelopes569lz@gmail.com>
2025-04-16 22:06:32 -0300

    Tela inicial

--- App.js
+++ App.js
@@ -1,10 +1,19 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image } from 'react-native';
+import LogoPega from './assets/PeganaTela.png'
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Image
+        source={LogoPega}
+        style={styles.logo}
+        resizeMode="contain"
+      />
+      <Text style={styles.nome}> PEGA NA TELA
+        
+      </Text>
+      
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +22,18 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#f5f5dc',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  logo: {
+    width: 80,
+    height: 80,
+  },
+  nome:{
+   color:'#000',
+   fontSize:15,
+   fontWeight:'bold',
+   marginBottom:10,
   },
 });
